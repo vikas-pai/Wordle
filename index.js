53e5c538f6d926cfea5c427ac07b503e0834d610
vikas-pai <paivikas1402@gmail.com>
2022-11-03 22:00:45 +0530

    added textboxes and implemented input logic

--- index.js
+++ index.js
@@ -11,4 +11,37 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}  
+}   
+var it=0;
+var arr=new Array(6);
+var arrind=0;
+var row='a';
+word=""
+ $("body").keydown(function(e) {
+  if(e.key=='Backspace')
+  {
+    $("."+row+it).css("border-color", "grey");
+    var bs=$("."+row+(it--)).val('');
+    word=word.substring(0, word.length - 1);
+    console.log(word);
+  }
+  if(e.keyCode>=65||e.keyCode>=122)
+  {  if(it<5)
+    {
+      var ad=$("."+row+(++it)).val(e.key);
+      word+=e.key;
+      $("."+row+it).css("border-color", "black");
+      console.log(word)
+    }
+  }
+  if(it==5&&e.key=='Enter')
+  {
+    it=0;
+    row=String.fromCharCode(row.charCodeAt() + 1)
+    arr[arrind]=word;
+    arrind++;
+    word="";
+    
+  }
+  console.log(arr);
+});
--- settings.js
+++ settings.js
@@ -5,5 +5,5 @@ $(".numletters").click(function(){
     console.log($(".numtriesoutput").html());
   });
   $('input[type="radio"]').on('click', function(e) {
-    alert(e.currentTarget.value);
+    console.log(e.currentTarget.value);
 });
