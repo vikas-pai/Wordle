e5a5ff49049a634de59a701a24a538a428d9ff8f
vikas-pai <paivikas1402@gmail.com>
2022-11-04 00:54:01 +0530

    center aligned the div and added styles to radio buttons and sliders

--- settings.js
+++ settings.js
@@ -7,3 +7,17 @@ $(".numletters").click(function(){
   $('input[type="radio"]').on('click', function(e) {
     console.log(e.currentTarget.value);
 });
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}   
