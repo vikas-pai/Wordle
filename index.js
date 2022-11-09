faa8561277f7d257d76484f65ee7b1d86bde161b
vikas-pai <paivikas1402@gmail.com>
2022-11-10 00:18:07 +0530

    loaded wordle input dynamically and corrected word validation logic

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ import { words5,words6,words7,words8,words9,words10 } from "./words.js";
 var numletters=sessionStorage.getItem("Letters");
 var diff=sessionStorage.getItem("Difficulty");
 var numtries=sessionStorage.getItem("Tries");
+var htmlString="";
+var row1='a';
+for (var i = 0; i < numtries; i++) {
+  for (var j = 1; j <=numletters; j++) {
+    htmlString=htmlString+"<input type=\"text\" name=\"words\" class=\""+row1+" "+row1+j+"\">";
+  }
+  row1=String.fromCharCode(row1.charCodeAt() + 1);
+  htmlString=htmlString+"<br>";
+}
+
+$(".wordlebox").html(htmlString)
 var words=[];
 switch (numletters) {
   case "5":words=words5;break;
@@ -12,6 +23,7 @@ switch (numletters) {
   case "10":words=words10;break;
   default:words=words5;break;
 }
+var currWord;
 switch (diff) {
   case "Easy":
     currWord=words[Math.floor(Math.random() * (3334 - 0) + 0)];break;
@@ -27,8 +39,70 @@ console.log(currWord);
 var occurCurrWord = new Array(26).fill(0);
 for (var i = 0; i < currWord.length; i++) {
   occurCurrWord[currWord[i].charCodeAt(0)-97]++;
-}
-console.log(occurCurrWord);
+} 
+$('input[type="text"], textarea').each(function(){
+  $(this).attr('readonly','readonly');
+});
+var it=0;
+var arr=new Array(numtries);
+var arrind=0;
+var row='a';
+var word=""
+ $("body").keydown(function(e) {
+  if(e.key=='Backspace')
+  {
+    $("."+row+it).css("border-color", "grey");
+    var bs=$("."+row+(it--)).val('');
+    word=word.substring(0, word.length - 1);
+  }
+  if(e.keyCode>=65&&e.keyCode<=122)
+  {  if(it<numletters)
+    {
+      var ad=$("."+row+(++it)).val(e.key);
+      word+=e.key;
+      $("."+row+it).css("border-color", "black");
+    }
+  }
+  if(it==numletters&&e.key=='Enter')
+  {
+    var occurWord = new Array(26).fill(0);
+    for (var i = 0; i < word.length; i++)
+    {
+      occurWord[word[i].charCodeAt(0)-97]++;
+    }
+    var occurCurrWord1=occurCurrWord.slice();
+    for (var j = 0; j < word.length; j++) 
+    {
+        if(word[j]==currWord[j])
+        {
+          $("."+row+(j+1)).css("border-color", "green");
+          occurWord[word[j].charCodeAt(0)-97]--;
+          occurCurrWord1[currWord[j].charCodeAt(0)-97]--;
+        }
+    }
+    for (var i = 0; i<26; i++) {
+      if(occurWord[i]>0&&occurCurrWord1[i]>0)
+      {
+        var cow=Math.min(occurWord[i],occurCurrWord1[i]);
+        for(var j=0;j<word.length&&cow>0;j++)
+        {
+          if(word[j]==String.fromCharCode("a".charCodeAt() + i)&&word[j]!=currWord[j])
+          {
+            $("."+row+(j+1)).css("border-color", "red");
+            cow--;
+          }
+        }
+      }
+      
+    }
+    it=0;
+    row=String.fromCharCode(row.charCodeAt() + 1)
+    arr[arrind]=word;
+    arrind++;
+    word="";
+    
+  }
+});
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
@@ -42,66 +116,4 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}   
-$('input[type="text"], textarea').each(function(){
-  $(this).attr('readonly','readonly');
-});
-var it=0;
-var arr=new Array(6);
-var arrind=0;
-var row='a';
-var word=""
- $("body").keydown(function(e) {
-  if(e.key=='Backspace')
-  {
-    $("."+row+it).css("border-color", "grey");
-    var bs=$("."+row+(it--)).val('');
-    word=word.substring(0, word.length - 1);
-    console.log(word);
-  }
-  if(e.keyCode>=65&&e.keyCode<=122)
-  {  if(it<5)
-    {
-      var ad=$("."+row+(++it)).val(e.key);
-      word+=e.key;
-      $("."+row+it).css("border-color", "black");
-      console.log(word)
-    }
-  }
-  if(it==5&&e.key=='Enter')
-  {
-    var occurWord = new Array(26).fill(0);
-    for (var i = 0; i < word.length; i++)
-    {
-      occurWord[word[i].charCodeAt(0)-97]++;
-    }
-    var occurCurrWord1=occurCurrWord;
-    for (var i = 0; i < currWord.length; i++) {
-      for (var j = 0; j < word.length; j++) {
-          if(word[j]==currWord[i]&&(i==j))
-          {
-            $("."+row+(i+1)).css("border-color", "green");
-            occurWord[word[j].charCodeAt(0)-97]--;
-            occurCurrWord1[currWord[i].charCodeAt(0)-97]--;
-          }
-          else if(word[j]==currWord[i])
-          {
-            if(occurCurrWord1[currWord[i].charCodeAt(0)-97]>0)
-            {
-              $("."+row+(j+1)).css("border-color", "red");
-              occurWord[word[j].charCodeAt(0)-97]--;
-              occurCurrWord1[currWord[i].charCodeAt(0)-97]--;
-            }
-          }
-      }
-      
-    }
-    it=0;
-    row=String.fromCharCode(row.charCodeAt() + 1)
-    arr[arrind]=word;
-    arrind++;
-    word="";
-    
-  }
-  console.log(arr);
-});
+} 
